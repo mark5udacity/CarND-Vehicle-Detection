2ced9a51a0bd81fdf1d8e5ceef5859fc40ff10b0
Mark 5 <markvudacity@gmail.com>
2017-09-27 22:43:42 -0400

    Add helper functions for hist, hog and other from the lessons.

--- python_funcs.py
+++ python_funcs.py
@@ -54,8 +54,87 @@ basedir = './data/non-vehicles'
 image_types = os.listdir(basedir)
 notcars = process_image_types(image_types, 'notcars.txt', 'Non-Vehicle')
 
-
 #### Next cell ####
 
+def base_hog(should_visualize, img, orient, pix_per_cell, cell_per_block):
+    return hog(
+            img,
+            orientations=orient,
+            pixels_per_cell=(pix_per_cell, pix_per_cell),
+            cells_per_block=(cell_per_block, cell_per_block),
+            transform_sqrt=False,
+            visualise=should_visualize,
+            feature_vector=False
+        )
+
+# Define a function to return HOG features and visualization
+def get_hog_features_experiment(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+    return base_hog(vis, orient, pix_per_cell, cell_per_block, feature_vec)
 
 
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+    if vis == True:
+        features, hog_image = hog(
+            img,
+            orientations=orient,
+            pixels_per_cell=(pix_per_cell, pix_per_cell),
+            cells_per_block=(cell_per_block, cell_per_block),
+            transform_sqrt=False,
+            visualise=True,
+            feature_vector=False
+        )
+        return features, hog_image
+    else:
+        features = hog(
+            img,
+            orientations=orient,
+            pixels_per_cell=(pix_per_cell, pix_per_cell),
+            cells_per_block=(cell_per_block, cell_per_block),
+            transform_sqrt=False,
+            visualise=False,
+            feature_vector=feature_vec
+        )
+        return features
+
+# Define a function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else:
+        feature_image = np.copy(img)
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(feature_image, size).ravel()
+    # Return the feature vector
+    return features
+
+
+# Define a function to compute color histogram features
+def color_hist(img, nbins=32): #, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel_1_hist = np.histogram(img[:, :, 0], bins=nbins)
+    channel_2_hist = np.histogram(img[:, :, 1], bins=nbins)
+    channel_3_hist = np.histogram(img[:, :, 2], bins=nbins)
+
+    # Generating bin centers
+    #bin_edges = channel_1_hist[1]
+    #bin_centers = (bin_edges[1:] + bin_edges[0:len(bin_edges) - 1]) / 2
+
+    hist_features = np.concatenate((channel_1_hist[0], channel_2_hist[0], channel_3_hist[0]))
+
+    return hist_features
+    #return channel_1_hist, channel_2_hist, channel_3_hist, bin_centers, hist_features
+
+#### Next cell ####
