142c9f784c94ddddd9828c098a40469aadb6f199
Mark 5 <markvudacity@gmail.com>
2017-09-28 00:47:11 -0400

    Adds sliding window with test.

--- python_funcs.py
+++ python_funcs.py
@@ -81,6 +81,17 @@ def correct_for_colorspace_or_copy(image, color_space):
 
     return feature_image
 
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+
 print('Loaded all utility functions')
 
 #### Next cell ####
@@ -192,11 +203,75 @@ def extract_features(
     return features
 print('Loaded Feature Extract Helper Functions!')
 
+#### Next cell ####
+
+# Sliding window and such!
+
+# Define a function that takes an image,
+# start and stop positions in both x and y,
+# window size (x and y dimensions),
+# and overlap fraction (for both x and y)
+def slide_window(
+        img,
+        x_start_stop=[None, None],
+        y_start_stop=[None, None],
+        xy_window=(64, 64),
+        xy_overlap=(0.5, 0.5)
+):
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start_stop[0] == None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] == None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] == None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] == None:
+        y_start_stop[1] = img.shape[0]
+    # Compute the span of the region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
+    nx_windows = np.int((xspan-nx_buffer)/nx_pix_per_step)
+    ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step)
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    # Note: you could vectorize this step, but in practice
+    # you'll be considering windows one by one with your
+    # classifier, so looping makes sense
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs*nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys*ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
+
+print('Loaded up sliding window functions.  Watch out for that banana peel!')
+
 
 #### Next cell ####
 # Testing stuff out
 
 OUTPUT_DIR = './output_images/{}'
+
+def show_or_save(output_file_name='did_not_supply_file_name.png'):
+    if platform == 'darwin':
+        print('Presuming to be on a mac, saving to file')
+        plt.savefig(OUTPUT_DIR.format(output_file_name))
+    else:
+        print('Presuming to be in Jupyter Notebook, calling show()')
+        plt.show()
+
 def run_feature_test(test_images, output_file_name='test_feature.png'):
 
     print('Testing out by picking randomly from {} images'.format(len(test_images)))
@@ -231,20 +306,32 @@ def run_feature_test(test_images, output_file_name='test_feature.png'):
         plt.title('Normalized Features')
         fig.tight_layout()
 
-        if platform == 'darwin':
-            print('Presuming to be on a mac, saving to file')
-            plt.savefig(OUTPUT_DIR.format(output_file_name))
-        else:
-            print('Presuming to be in Jupyter Notebook, calling show()')
-            plt.show()
+        show_or_save(output_file_name)
 
+
+def run_sliding_windows_test(test_images, output_file_name='sliding_window_test.png'):
+    jpg_img_idx = np.random.randint(1, 7)
+    image = mpimg.imread('./test_images/test{}.jpg'.format(jpg_img_idx))
+
+    windows = slide_window(
+        image,
+        x_start_stop=[None, None],
+        y_start_stop=[None, None],
+        xy_window=(128, 128),
+        xy_overlap=(0.5, 0.5)
+    )
+
+    window_img = draw_boxes(image, windows, color=(155, 55, 255), thick=6)
+    plt.figure()
+    plt.imshow(window_img)
+    show_or_save(output_file_name)
 
 if platform != 'darwin':  # Mac OSX
     print('Only meant for running from command line!  Or maybe not?')  #TODO: Verify from Jupyter if needed, think it will work...
 else:
     test_images = glob.glob('./test_images/*.png')
-    run_feature_test(test_images, 'test.png')
-
+    run_feature_test(test_images, 'feature_test.png')
+    run_sliding_windows_test(test_images)
     del test_images
 
 print("All done testing!  How's it lookin'!?")
