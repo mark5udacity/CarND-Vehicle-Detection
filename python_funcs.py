97e11cc3d856b7a2e26961ab13529cf3166edbe5
Mark 5 <markvudacity@gmail.com>
2017-09-28 21:32:09 -0400

    Playing with HOG parameters, pt 2

--- python_funcs.py
+++ python_funcs.py
@@ -236,13 +236,13 @@ def hog_params(feature_image, orient, pix_per_cell, cell_per_block, feature_vec)
 def single_img_features(
         image,
         color_space='YCrCb',
-        hist_bins=32,
+        hist_bins=16,
         hist_feat=True,
         hog_channel=ALL_HOG_CHANNELS,
         hog_feat=True,
-        cell_per_block=2,
+        cell_per_block=3,
         orient=9,
-        pix_per_cell=8,
+        pix_per_cell=7,
         spatial_feat=True,
         spatial_size=(16, 16),
         viz=False
@@ -500,11 +500,11 @@ def run_window_search_test(test_images, output_file_name='search_slide_test.png'
     ### TODO: Tweak these parameters and see how the results change.
     color_space = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
     orient = 9  # HOG orientations
-    pix_per_cell = 7  # HOG pixels per cell
-    cell_per_block = 3  # HOG cells per block
+    pix_per_cell = 8  # HOG pixels per cell
+    cell_per_block = 2  # HOG cells per block
     hog_channel = ALL_HOG_CHANNELS  # Can be 0, 1, 2, or "ALL"
     spatial_size = (16, 16)  # Spatial binning dimensions
-    hist_bins = 12  # Number of histogram bins
+    hist_bins = 16  # Number of histogram bins
     spatial_feat = True  # Spatial features on or off
     hist_feat = True  # Histogram features on or off
     hog_feat = True  # HOG features on or off
